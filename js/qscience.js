3ce3155321c365fbb2078b261516aa232b4167b7
kojak <tamas.kojedzinszky@gmail.com>
2012-10-17 17:10:57 +0200

    started to develop citations addition to paper

--- js/qscience.js
+++ js/qscience.js
@@ -20,6 +20,15 @@
             addAuthor(i+1, Drupal.settings.pdfparser.authors[i]);
           }
         }
+        //console.log(Drupal.settings.pdfparser.citations);
+        if (Drupal.settings.pdfparser.citations) {
+          for (var i = 0; i < Drupal.settings.pdfparser.citations.length; i++) {
+            var title = Drupal.settings.pdfparser.citations[i].title;
+            if (title.length > 0) {
+              addCitation(i+1, title);
+            }
+          }
+        }
       }
     } else if (settings.extraData._triggering_element_name == 'field_paper_upload_und_0_remove_button') {
       $('#edit-title').val('');
@@ -32,6 +41,13 @@
         i++;
       }
       jQuery('div.form-item-author-1 input').val('');
+      i = 2;
+      while (jQuery('div.form-item-citation-'+i).length != 0) {
+        jQuery('div.form-item-citation-'+i).remove();
+        i++;
+      }
+      jQuery('div.form-item-citation-1 input').val('');
+
     }
   });
 })(jQuery);
@@ -70,6 +86,40 @@ function makeAuthorCopy(num, name) {
   copy.find('#edit-author-1').attr('id', 'edit-author-'+num).attr('name', 'author_'+num);
 
   jQuery('#edit-author-adder').before(copy);
+}
 
-  
+function addCitation(i, name) {
+  var num = 1;
+  while (true) {
+    if (jQuery('div.form-item-citation-'+num).length == 0) {
+      break;
+    } else {
+      num++;
+    }
+  }
+  if (typeof i != 'undefined' && typeof name != 'undefined') {
+    if (i >= num) {
+      makeCitationCopy(num, name);
+    } else {
+      jQuery('div.form-item-citation-'+i+' input').val(name);
+    }
+  } else {
+    makeCitationCopy(num);
+  }
+
+  return false;
+}
+
+function makeCitationCopy(num, name) {
+  var copy = jQuery('div.form-item-citation-1').clone();
+  if (typeof name != 'undefined') {
+    copy.find('input').val(name);
+  } else {
+    copy.find('input').val('');
+  }
+  copy.removeClass('form-item-citation-1').addClass('form-item-citation-'+num);
+  copy.find('label').attr('for', 'edit-citation-'+num);
+  copy.find('#edit-citation-1').attr('id', 'edit-citation-'+num).attr('name', 'citation_'+num);
+
+  jQuery('#edit-citation-adder').before(copy);
 }
